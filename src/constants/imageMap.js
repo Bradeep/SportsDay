75727716b01738c8f42abb49543222f6f21e312c
Bradeep <bradeep001@gmail.com>
2023-09-16 15:46:10 +0530

    Adding constants and util files

---
+++ src/constants/imageMap.js
@@ -0,0 +1,15 @@
+export const sportImages = {
+  athletics: require("../assets/images/athletics.jpg"),
+  cricket: require("../assets/images/cricket.jpeg"),
+  football: require("../assets/images/football.jpeg"),
+  boxing: require("../assets/images/boxing.jpeg"),
+  swimming: require("../assets/images/swimming.jpeg"),
+};
+
+export const sportIcons = {
+  athletics: require("../assets/icons/athletics.svg").default,
+  cricket: require("../assets/icons/cricket.svg").default,
+  football: require("../assets/icons/football.svg").default,
+  boxing: require("../assets/icons/boxing.svg").default,
+  swimming: require("../assets/icons/swimming.svg").default,
+};
---
+++ src/utilities/utils.js
@@ -0,0 +1,42 @@
+export const toStandardTime = (dateTime) => {
+  const date = new Date(dateTime);
+
+  return date.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+};
+
+export const formattedDate = (dateTime) => {
+  const today = new Date(dateTime);
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1;
+  let dd = today.getDate();
+
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+
+  const formattedDate = dd + "/" + mm + "/" + yyyy;
+
+  return formattedDate;
+};
+
+export const isOverLapping = (selectedEvents, currEvent) => {
+  var overlapping = selectedEvents.some((event) => {
+    if (
+      new Date(currEvent.start_time) >= new Date(event.start_time) &&
+      new Date(currEvent.start_time) < new Date(event.end_time)
+    )
+      return true;
+    else if (
+      new Date(currEvent.end_time) > new Date(event.start_time) &&
+      new Date(currEvent.end_time) <= new Date(event.end_time)
+    )
+      return true;
+
+    return false;
+  });
+
+  return overlapping;
+};
