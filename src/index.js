fd26562ff2e06bc4a35873caeaadde868a64610e
Bradeep <bradeep001@gmail.com>
2023-09-17 13:56:33 +0530

    Added test cases for error boundary

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import SportsDay from "./views";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,4 +9,4 @@ root.render(<SportsDay />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
